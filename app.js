9ca0eea38da3dfa9f94e9f85d2b1a97b873e0cc1
nadun <n.nadun@theglobalmarketaccess.com>
2021-02-01 18:20:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,135 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require ("express");
+const bodyParser = require ("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require("passport-local-mongoose");
+
+
+const port = 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false 
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//mongoose
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    secret: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model('user', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+///////////////////////////////////////// get requests ////////////////////////////////////////
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register")
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+app.get("/secrets", function(req, res){
+    User.find({'secret':{$ne: null}},function(err, foundUsers){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets", {usersWithSecrets: foundUsers});
+        }
+    })
+});
+
+app.get("/submit", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    }else{
+        res.redirect("/login");
+    }
+});
+/////////////////////////////////////// post requests /////////////////////////////////////////
+app.post("/register", function(req, res){
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.render("/register");
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            })
+        }
+    });
+
+});
+
+app.post("/login", function(req, res){    
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+    
+    User.findById(req.user.id, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                console.log("user found!");
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    })
+});
+
+
+app.listen(port, function(){
+    console.log("Server is running on :/localhost:"+port);
+})
